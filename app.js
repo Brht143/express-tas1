7c81f2eef4514f712f91daaeb8e93d52ff89271c
Ibrahim Chami <i.h.elchami@gmail.com>
2024-12-02 19:54:42 +0300

    Intro to express

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.json({ name: "Ibrahim" }));
+app.get("/", (req, res) => res.send({ name: "Ibrahim" }));
 
 app.listen(port, () =>
-  console.log("The application is running on localhost:8000")
+  console.log(`The application is running on localhost:${port}`)
 );
